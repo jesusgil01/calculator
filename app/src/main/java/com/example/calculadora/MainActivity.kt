4517d890b96a66e119bb50e9764e779b8d2b59b9
Jesús Gil <jesus.gil2001@gmail.com>
2021-02-13 18:58:42 -0700

    Se utilizaron Buttons para todas las operaciones, falta corregir operacion log

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -6,18 +6,21 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlin.math.E
-import kotlin.math.log
-import kotlin.math.sin
-import kotlin.math.sqrt
+import org.w3c.dom.Text
+import kotlin.math.*
 
 const val SUMA = 1
 const val RESTA = 2
 const val MULTIPLICACION = 3
 const val DIVISION = 4
-const val SQRT = 5;
-const val LOG = 6;
-const val SIN = 7;
+const val SQRT = 5
+const val LOG = 6
+const val SIN = 7
+const val COS = 8
+const val TAN = 9
+const val LN = 10
+const val POW = 11
+const val FACT = 12
 const val NINGUNA = 0
 
 class MainActivity : AppCompatActivity() {
@@ -35,10 +38,73 @@ class MainActivity : AppCompatActivity() {
     fun numeroPresionado(view: View) {
         val num = findViewById<Button>(view.id).text
         val txtInput = findViewById<TextView>(R.id.txtInput)
-        var res = 0.00
         if (txtInput.text.toString().startsWith("0"))
             txtInput.setText("$num")
         else
             txtInput.setText("${txtInput.text}$num")
+        if (operacion == NINGUNA)
+            num1 = txtInput.text.toString().toDouble()
+        else
+            num2 = txtInput.text.toString().toDouble()
+        resultado = false
     }
+
+    fun operacionPresionada(view: View) {
+        val op = findViewById<Button>(view.id).text
+        val txtSign = findViewById<TextView>(R.id.txtSign)
+        val txtInput = findViewById<TextView>(R.id.txtInput)
+        txtSign.setText("$op")
+        if (resultado) num1= txtInput.text.toString().toDouble()
+        when (op) {
+            "+" -> operacion = SUMA
+            "-" -> operacion = RESTA
+            "x" -> operacion = MULTIPLICACION
+            "/" -> operacion = DIVISION
+            "√" -> operacion = SQRT
+            "log" -> operacion = LOG
+            "sin" -> operacion = SIN
+            "cos" -> operacion = COS
+            "tan" -> operacion = TAN
+            "ln" -> operacion = LN
+            "^" -> operacion = POW
+            "!" -> operacion = FACT
+        }
+        num1 = txtInput.text.toString().toDouble()
+        txtInput.setText("0")
+    }
+
+    fun igualPresionado(view: View) {
+        val txtInput = findViewById<TextView>(R.id.txtInput)
+        val res = when(operacion) {
+            SUMA -> num1 + num2
+            RESTA -> num1 - num2
+            MULTIPLICACION -> num1 * num2
+            DIVISION -> {
+                if (num2 != 0.00)
+                    num1 / num2
+                else
+                    "ERROR"
+            }
+            SQRT -> sqrt(num2)
+            LOG -> log(num2, E)
+            SIN -> sin(num2)
+            COS -> cos(num2)
+            TAN -> tan(num2)
+            LN -> ln(num2)
+            POW -> num1.pow(num2)
+            FACT -> factorial(num2)
+            else -> 0
+        }
+        txtInput.setText("$res")
+        print("dummy")
+    }
+
+    fun factorial(num: Double): Double {
+        if (num >= 1)
+            return num * factorial(num - 1)
+        else
+            return 1.00
+    }
+
+
 }
